928e08e7b52d371791da9b5cf6ccddb5ec426c01
jkushnir <jkushnir@me.com>
2020-09-12 14:36:14 +0300

    e2e updates and Dockerfile

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,43 +1,44 @@
 #!/usr/bin/env python3
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys 
-import time
-import os
 import sys
-from datetime import datetime
-import logging
 
 
-app_url = 'http://0.0.0.0:8777/'
-
-
-options = webdriver.ChromeOptions()
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-gpu')
-#options.add_argument("--headless")
-
-driver = webdriver.Chrome(options=options)
-driver.implicitly_wait(10)
-
+# function to retrieve number from the score element in the provided webpage and check if it's between 0 and 1000. Return a boolean value on the result.
 def test_scores_service(app_url):
+    # setting initial return state to False. Will turn to True only if conditions are met without exception errors.
+    is_score_valid = False
+
+    # score element xpath
     score_element = '//*[@id="score"]'
-    score_valid = False
+
+    options = webdriver.ChromeOptions()
+    # below options are for running the webdriver in a docker container
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument("--headless")
+
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(10)
+
+    # using "try" to get to return regardless of the exception type
     try:
         driver.get(app_url)
-        score = driver.find_element_by_xpath(score_element)
-        if (int(score) > 0) or (int(score < 1000):
-            score_valid = True
-            print('success!!!')
+        score = driver.find_element_by_xpath(score_element).text
+        print(score)
+        if int(score) >= 0 and int(score) <= 1000:
+            is_score_valid = True
     except Exception as e:
         print(e)
-        driver.quit()
     driver.quit()
-driver.quit()
-    return score_valid
+    return is_score_valid
 
 def main_function():
-    return os_code
+    app_url = 'http://0.0.0.0:8777/'
+    if test_scores_service(app_url) == True:
+        sys.exit(0)
+    else:
+        sys.exit(-1)
+    return
 
 if __name__ == "__main__":
     main_function()
